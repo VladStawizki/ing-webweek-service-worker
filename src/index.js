f8833bf6431caac8addb497c86d3c80e655192d4
Florian Unglaub 🤡 <florian.unglaub@ing.de>
2019-07-12 09:51:25 +0200

    onResize

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+let imageWidth =  Math.round(window.innerWidth / 3);
+
+const onResize = (ev) => {
+  const width = ev.currentTarget.innerWidth;
+
+  imageWidth = Math.round(width / 3);
+  console.log(imageWidth);
+}
+
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js');
 
   navigator.serviceWorker.ready
   .then( (registration) => {
     if (registration.active) {
-      registration.active.postMessage(200);
 
+      window.onresize = onResize;
+
+      registration.active.postMessage(imageWidth);
+
+      // Cheap image lazy loading
       document.querySelectorAll('img[data-src]').forEach((image) => {
         image.setAttribute('src', image.getAttribute('data-src'));
       });
